a10bbd7536524e0a011a9d1f5517f9fb5f005285
Lumnyl <scuram@hotmail.fr>
2024-06-25 12:42:30 +0200

    fix new

--- doom_concepts/script.js
+++ doom_concepts/script.js
@@ -103,7 +103,7 @@ function build_weapon_table() {
         weapon_head.appendChild(cell)
     }
     for (let weapon of weapon_list) {
-        if (subcategory_selector.value == 0 || weapon_categ_list[subcategory_selector.value-1] == weapon.category || (weapon_categ_list[subcategory_selector.value] == 1 || weapon.isnew)) {
+        if (subcategory_selector.value == 0 || weapon_categ_list[subcategory_selector.value-1] == weapon.category || (weapon_categ_list[subcategory_selector.value-1] == "Disposable" && weapon.category == "Partially Disposable") || (subcategory_selector.value == 1 && weapon.isnew)) {
             let row = document.createElement("tr")
             for (let item of head_lists.weapons) {
                 let cell = document.createElement("td")
