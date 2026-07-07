77776900caa8e23e624f89621bd6e6ac369eb82b
Lumnyl <scuram@hotmail.fr>
2026-07-07 22:43:32 +0200

    c

--- doom_concepts/weapons.js
+++ doom_concepts/weapons.js
@@ -1422,7 +1422,7 @@ export default [
         "notes" : "Requires reloading, which throws the weapons like grenades and discards all leftover ammunition. Grenade damage increases how much ammo was discarded that way.",
         "superior" : "Causes enemies to drop Life Essence on death.",
         "durability": false,
-        "description": "A twisted pair of dark shotguns possessed with rage and firing pure hatred. No need to reload, just throw them away and they'll teleport to your hands fully loaded. Each gun has its named engraved onto it, in japanese obviously.",
+        "description": "A twisted pair of dark shotguns possessed with rage and firing pure hatred. No need to reload, just throw them away and they'll teleport to your hands fully loaded. Each gun has its named engraved onto it in japanese, because of course it is.",
         "authorsnotes": "",
         "designnotes": "",
         "newstatus": 1
